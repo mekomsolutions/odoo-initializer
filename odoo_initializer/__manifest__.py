2a8a34fcf45e29d7d29b4c0fe7dc494efaf20cf2
rbuisson <romain@mekomsolutions.com>
2019-02-05 16:24:30 -0400

    HSC-16: Configure build steps with Gradle

--- odoo_initializer/__manifest__.py
+++ odoo_initializer/__manifest__.py
@@ -1,16 +1,26 @@
+# -*- coding: utf-8 -*-
 {
-    "name": "initz",
-    "summary": """
-        Odoo metadata/data initializer""",
-    "description": """
-        an odoo module to import data and metadata provided by openmrs config
+    'name': "Odoo initializer",
+
+    'summary': """
+        Odoo data initializer""",
+
+    'description': """
+        Odoo add-on to import data provided as CSVs, loaded from a location on the server disk.
+        This add-on helps implementers manage/deploy some of the Odoo data as files so to easily replicate environments.
+        This add-on also specifically supports loading products and prices from an OpenMRS Initializer style CSV.
+        See https://github.com/mekomsolutions/openmrs-module-initializer/
     """,
-    "author": "MekomSolutions",
-    "website": "http://www.mekomsolutions.com",
-    "category": "Uncategorized",
-    "version": "0.1",
+
+    'author': "Mekom Solutions",
+    'website': "http://www.mekomsolutions.com",
+
+    'category': 'Technical Settings',
+    'version': '1.0-SNAPSHOT',
+
     "depends": ["base", "base_import"],
     "external_dependencies": {"python" : ["odoorpc"]},
+
     "application": True,
     "installable": True,
     "auto_install": True
