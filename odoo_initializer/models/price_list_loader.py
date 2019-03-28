f6c78a280ae69121443fd37df6422767bc2ff3f1
chine zoheir <chine.zoheir@gmail.com>
2019-03-28 20:09:54 +0800

    HSC-16: Update CSV test files structure (#6)

--- odoo_initializer/models/price_list_loader.py
+++ odoo_initializer/models/price_list_loader.py
@@ -7,7 +7,7 @@ _logger = logging.getLogger(__name__)
 
 class PriceListLoader(BaseModelLoader):
     update_existing_record = True
-    data_files_source = "openmrs"
+    data_files_source = "odoo"
     model_name = "product.pricelist"
     folder = "pricelist"
 
