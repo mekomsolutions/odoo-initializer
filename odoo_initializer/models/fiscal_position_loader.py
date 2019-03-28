d9af9423ca2334da9cec1fa17f50504ad383e7a1
chinzou <chine.zoheir@gmail.com>
2019-03-28 18:46:37 +0800

    HSC-16: Update csv files structure

--- odoo_initializer/models/fiscal_position_loader.py
+++ odoo_initializer/models/fiscal_position_loader.py
@@ -5,5 +5,5 @@ class FiscalPositionLoader(BaseModelLoader):
     update_existing_record = True
     data_files_source = "odoo"
     model_name = "account.fiscal.position"
-    folder = "fiscal_postion"
+    folder = "fiscal_position"
     filters = {}
