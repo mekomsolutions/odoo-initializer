fb821de748dbbc5e84fa170dae758bcd7d9f5027
chine zoheir <chine.zoheir@gmail.com>
2019-04-03 22:01:18 +0800

    HSC-16: Add company property loader (#8)

--- odoo_initializer/models/drug_loader.py
+++ odoo_initializer/models/drug_loader.py
@@ -1,15 +1,15 @@
-import logging
-
 from .base_loader import BaseModelLoader
-
-_logger = logging.getLogger(__name__)
 
 
 class DrugLoader(BaseModelLoader):
     update_existing_record = True
     data_files_source = "openmrs"
     model_name = "product.template"
-    field_mapping = {"id": "id", "name": "name", "drug": "Fully specified name:en", "lst_price": "odoo_price"}
+    field_mapping = {
+        "id": "id",
+        "name": "name",
+        "drug": "Fully specified name:en",
+        "lst_price": "odoo_price",
+    }
     folder = "drugs"
     filters = {}
-
