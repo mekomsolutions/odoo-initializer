4423c9d9cd39616ac43e01fe856ac9f632a47d50
chinzou <chine.zoheir@gmail.com>
2019-04-03 15:21:53 +0800

    HSC-16: Add company property loader

--- odoo_initializer/activator.py
+++ odoo_initializer/activator.py
@@ -1,5 +1,6 @@
 import logging
 
+from .models.company_property_loader import CompanyPropertyLoader
 from .models.stock_location_loader import StockLocationLoader
 from .models.payment_term_loader import PaymentTermLoader
 from .models.price_list_loader import PriceListLoader
@@ -11,10 +12,18 @@ _logger = logging.getLogger(__name__)
 
 _logger.info("start initialization process")
 
-registered_loader = [FiscalPositionLoader, JournalLoader, PaymentTermLoader, StockLocationLoader , DrugLoader, PriceListLoader]
+registered_loaders = [
+    FiscalPositionLoader,
+    JournalLoader,
+    PaymentTermLoader,
+    StockLocationLoader,
+    DrugLoader,
+    PriceListLoader,
+    CompanyPropertyLoader,
+]
 
-for loader_class in registered_loader:
-    loader = loader_class()
+for registered_loader in registered_loaders:
+    loader = registered_loader()
     loader.load_()
 
 
--- odoo_initializer/models/company_property_loader.py
+++ odoo_initializer/models/company_property_loader.py
@@ -1,9 +1,9 @@
 from .base_loader import BaseModelLoader
 
 
-class StockLocationLoader(BaseModelLoader):
+class CompanyPropertyLoader(BaseModelLoader):
     update_existing_record = True
     data_files_source = "odoo"
-    model_name = "stock.location"
-    folder = "stock_location"
+    model_name = "ir.property"
+    folder = "company_property"
     filters = {}
