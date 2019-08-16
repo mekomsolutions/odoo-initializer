9c480df47c75944f28ef10bed43e790d7067d766
chinzou <chine.zoheir@gmail.com>
2019-08-16 15:33:04 +0800

    HSC-16: Update drugs field mapping

--- odoo_initializer/models/drug_loader.py
+++ odoo_initializer/models/drug_loader.py
@@ -4,10 +4,11 @@ from .base_csv_loader import BaseCsvLoader
 class DrugLoader(BaseCsvLoader):
     model_name = "product.template"
     field_mapping = {
-        "id": "id",
-        "name": "name",
-        "drug": "Fully specified name:en",
+        "id": "Uuid",
+        "name": "Name",
+        "drug": "Concept Drug",
         "lst_price": "odoo_price",
+        "categ_id/id": "odoo_category",
     }
     folder = "drugs"
     filters = {}
