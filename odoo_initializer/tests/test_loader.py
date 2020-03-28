fe010e95643a28b88a99026ee14861f65bfc00de
chinzou <chine.zoheir@gmail.com>
2020-03-29 00:49:06 +0800

    BAHMNI-71: Add tests for Loader rules

--- odoo_initializer/tests/test_loader.py
+++ odoo_initializer/tests/test_loader.py
@@ -8,16 +8,23 @@ _logger = logging.getLogger(__name__)
 
 class TestLoader(tests.TransactionCase):
 
-    _test_model = "res.partner"
+    _test_model = "res.groups"
 
     @staticmethod
-    def _get_unfiltered_partners():
+    def _get_groups():
         return [
-            {"name": "test_1", "website": "example.com"},
-            {"name": "test_2", "website": "example.com"},
-            {"name": "test_3", "website": "test.com"},
-            {"name": "test_4", "website": "example.com"},
-            {"name": "test_4", "website": "test.com"},
+            {"id": "group_test_1", "name": "test_1", "comment": "other"},
+            {"id": "group_test_2", "name": "test_2", "comment": "filter"},
+            {"id": "group_test_3", "name": "test_3", "comment": "other"},  # , "invoice_warn": "No Message"
+        ]
+
+    @staticmethod
+    def _get_partners():
+        return [
+            {"id": "partner_1",
+             "name": "partner 1",
+             "local_name": "refer to group:${group_test_1}"
+             }
         ]
 
     def test_filter_applied(self):
@@ -25,14 +32,54 @@ class TestLoader(tests.TransactionCase):
         # Setup
         test_loader = BaseLoader()
         test_loader.model_name = self._test_model
-        test_loader.identifier = "name"
-        test_loader.test = True
-        test_loader.filters = {"website": "example.com"}
+        test_loader.filters = {"comment": "filter"}
 
         # Replay
-        filtered_partners = test_loader._mapper(
-            self._get_unfiltered_partners(), None, test_loader.filters
+        filtered_groups = test_loader._mapper(
+            self._get_groups(), None, test_loader.filters
         )
 
         # Verify
-        assert "test.com" not in filtered_partners
+        assert "other" not in filtered_groups
+
+    def test_no_update_rule_should_not_update_comment(self):
+
+        # Setup
+        test_loader = BaseLoader()
+        test_loader.model_name = self._test_model
+        test_loader.field_rules = {
+            "comment": "NO_UPDATE"
+        }
+        test_loader.fields = ["id", "name", "comment"]
+
+        # Replay
+        test_loader.load_file(test_loader._mapper(
+            self._get_groups(), None, None)
+        )
+        filtered_groups = test_loader._mapper(
+            self._get_groups(), None, None
+        )
+
+        # Verify
+        assert "comment" not in filtered_groups
+
+    def test_external_to_internal_id_rule_should_replace_group_id(self):
+
+        # Setup
+        test_loader = BaseLoader()
+        test_loader.model_name = self._test_model
+        test_loader.fields = ["id", "name", "comment"]
+        test_loader.load_file(test_loader._mapper(
+            self._get_groups(), None, None)
+        )
+        test_loader.field_rules = {
+            "local_name": "EXTERNAL_TO_INTERNAL_ID"
+        }
+
+        # Replay
+        filtered_partners = test_loader._mapper(
+            self._get_partners(), None, None
+        )
+
+        # Verify
+        assert filtered_partners.split("refer to group:", 1)[1].split(",")[0].rstrip().isdigit()
