1874b8dc918a27ca2e4965220f4b165b7c54dd3d
rbuisson <romain@mekomsolutions.com>
2020-05-05 12:12:23 +0200

    Next development: 1.1.0-SNAPSHOT

--- odoo_initializer/__manifest__.py
+++ odoo_initializer/__manifest__.py
@@ -16,7 +16,7 @@
     'website': "http://www.mekomsolutions.com",
 
     'category': 'Technical Settings',
-    'version': '1.0.0',
+    'version': '1.1.0-SNAPSHOT',
 
     "depends": ["base", "base_import"],
 
