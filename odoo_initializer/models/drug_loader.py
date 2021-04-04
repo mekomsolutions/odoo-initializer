40f952e5e4cc855775be19b0874dbb3e226d31ad
chinzou <chine.zoheir@gmail.com>
2021-04-04 21:08:21 +0300

    INFRA-260: Add 'purchase uom' to drugs loader

--- odoo_initializer/models/drug_loader.py
+++ odoo_initializer/models/drug_loader.py
@@ -11,7 +11,8 @@ class DrugLoader(BaseLoader):
         "product_variant_ids/uuid": "Uuid",
         "id": "odoo_id",
         "uom_id/id": "odoo_uom",
-        "standard_price": "odoo_cost"
+        "standard_price": "odoo_cost",
+        "uom_po_id/id": "odoo_purchase_uom"
     }
     folder = "drugs"
     filters = {}
