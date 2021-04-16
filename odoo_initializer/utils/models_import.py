7ec2562375f526ca976f028a1dfa437715c5331a
chinzou <chine.zoheir@gmail.com>
2021-04-16 13:15:47 +0300

    INFRA-276: Models loader to ignore incorrect configuration file

--- odoo_initializer/utils/models_import.py
+++ odoo_initializer/utils/models_import.py
@@ -1,5 +1,6 @@
 import logging
 import json
+import os
 
 from ..models.base_loader import BaseLoader
 from .config import config
@@ -22,16 +23,29 @@ class ModelsImport:
     def get_iniz_config_file_models(self):
         if (not config.config_file_path):
             return ""
+        if (not os.path.exists(config.config_file_path)):
+            _logger.warn("Configuration file is specified but does not exists")
+            return ""
         with open(config.config_file_path) as json_file:
-            data = json.load(json_file)
+            data = ""
+            try:
+                data = json.load(json_file)
+            except ValueError:
+                _logger.warn("Configuration file is not a json file, skipping")
+                return ""
+
             config_loaders = []
 
-            for model in data['models']:
-                loader = BaseLoader()
-                loader.folder = self.get_property_value(model, "folder", True)
-                loader.model_name = self.get_property_value(model, "model_name", True)
-                loader.field_mapping = self.get_property_value(model, "field_mapping")
-                loader.filters = self.get_property_value(model, "filters")
-                loader.field_rules = self.get_property_value(model, "field_rules")
-                config_loaders.append(loader)
-            return config_loaders
+            try:
+                for model in data['models']:
+                    loader = BaseLoader()
+                    loader.folder = self.get_property_value(model, "folder", True)
+                    loader.model_name = self.get_property_value(model, "model_name", True)
+                    loader.field_mapping = self.get_property_value(model, "field_mapping")
+                    loader.filters = self.get_property_value(model, "filters")
+                    loader.field_rules = self.get_property_value(model, "field_rules")
+                    config_loaders.append(loader)
+                return config_loaders
+            
+            except KeyError:
+                _logger.warn("Configuration file does not have models section")
