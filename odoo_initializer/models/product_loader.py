839853316d5d32cb5ca2c0ebaf2419d1d7e415e8
chinzou <chine.zoheir@gmail.com>
2021-06-28 04:12:02 +0300

    C2C-92: Add filters to 'product' loader

--- odoo_initializer/models/product_loader.py
+++ odoo_initializer/models/product_loader.py
@@ -5,3 +5,7 @@ class ProductLoader(BaseLoader):
     model_name = "product.template"
     folder = "product"
     filters = {}
+    field_rules = {
+            "lst_price": "NO_UPDATE",
+            "standard_price": "NO_UPDATE"
+        }
