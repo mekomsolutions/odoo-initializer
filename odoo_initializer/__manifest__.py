f528bf740a0b7a23925fc1ba90d22bca80143eb9
Zoheir CHINE <zoheir@mekomsolutions.com>
2022-05-09 12:23:00 +0000

    Next development: 2.1.0-SNAPSHOT

--- odoo_initializer/__manifest__.py
+++ odoo_initializer/__manifest__.py
@@ -16,7 +16,7 @@
     'website': "http://www.mekomsolutions.com",
 
     'category': 'Technical Settings',
-    'version': '2.0.0',
+    'version': '2.1.0-SNAPSHOT',
 
     "depends": ["base", "base_import"],
 
