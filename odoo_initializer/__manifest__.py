8be773c2bb05e07db80a437be40e355e933cf8ab
rbuisson <romain@mekomsolutions.com>
2022-06-29 11:01:00 +0200

    Release 2.1.0

--- odoo_initializer/__manifest__.py
+++ odoo_initializer/__manifest__.py
@@ -16,7 +16,7 @@
     'website': "http://www.mekomsolutions.com",
 
     'category': 'Technical Settings',
-    'version': '2.1.0-SNAPSHOT',
+    'version': '2.1.0',
 
     "depends": ["base", "base_import"],
 
