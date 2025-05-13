ab71501e0508f44789adbb1859032e0d6d82a480
corneliouzbett <kipchubett@gmail.com>
2025-05-13 14:49:58 +0300

    Update addon version to 17.0.0.0.1

--- odoo_initializer/__manifest__.py
+++ odoo_initializer/__manifest__.py
@@ -16,7 +16,7 @@
     'website': "http://www.mekomsolutions.com",
 
     'category': 'Technical Settings',
-    'version': '17.0.0.0.0',
+    'version': '17.0.0.0.1',
 
     "depends": ["base", "base_import"],
 
